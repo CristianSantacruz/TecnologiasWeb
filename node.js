f6014eafe094b99197302b36857891a64bdb9a34
CristianSantacruz <santacruz-guarquila@hotmail.com>
2016-11-23 14:35:28 -0500

    Commit1

---
+++ node.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    imprimirSuma1y2: imprimirSuma1y2;
+}
+
+function sumar1y2 (){
+    customField: 'CustonField'
+    var numero1 = 1;
+    numero2 = 2;
+    
+    console.log(numero1 + numero2);
+}
