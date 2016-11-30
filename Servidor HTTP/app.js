dba7c8c3fb4b735468de03cbf49f5597f0067384
CristianSantacruz <santacruz-guarquila@hotmail.com>
2016-11-30 14:59:18 -0500

    Nuevo Usuario

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -3,7 +3,9 @@ var app = express();
 
 var usuarios = [{ id:1, nombre: 'Pepe', cedula: '12345' },
                 { id:2, nombre: 'Juan', cedula: '12345'},    
-                { id:3, nombre: 'Pedro', cedula: '12345'}]
+                { id:3, nombre: 'Pedro', cedula: '12345'}];
+
+var contador = 3;
 
 
 app.get('/', function (req, res) {
@@ -26,6 +28,24 @@ app.post('/TecnologiasWeb/:name', function (req, res) {
     } 
     
     res.send('No existe el Usuario');
+});
+
+
+app.get('/Usuarios', function (req, res) {
+  
+    if(!req.query.nombre){
+        res.send('No envió nombre');
+    }
+    
+    if(!req.query.cedula){
+        res.send('No envió cédula');
+    }
+    
+    var nuevoUsuario = { id:contador+1, nombre:req.query.nombre, cedula:req.query.cedula };
+    usuarios.push(nuevoUsuario);
+    contador = contador++;
+    res.json(nuevoUsuario);
+    
 });
 
 app.listen(5050, function () {
