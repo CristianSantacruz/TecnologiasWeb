705a8b4d2563b942b3d9942a92c55195bad7a3c8
CristianSantacruz <santacruz-guarquila@hotmail.com>
2016-11-30 13:51:02 -0500

    con POST

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -2,8 +2,20 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Tecnologias Web con "GET"');
+  res.send('Hola Mundo');
 });
+
+
+app.get('/TecnologiasWeb', function (req, res) {
+  res.send('con JavaScript');
+});
+
+
+app.post('/TecnologiasWeb', function (req, res) {
+  res.send('con JavaScript');
+});
+
+
 
 app.listen(5050, function () {
   console.log('Escuchando en puerto5050');
