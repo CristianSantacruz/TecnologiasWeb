5007f5ddcfd56916ae86000568aed0dfe488766d
CristianSantacruz <santacruz-guarquila@hotmail.com>
2016-12-07 14:39:29 -0500

    Commit3

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-var fs require(fs);
+var fs = require('fs');
 
 //Primer paramatro es el path de la pagina html y el segundo parametro es la codificación
-fs.readFile ('./Paginas/pagina.html', 'utf8', function(err, archivoLeido){ console.log(error);
+fs.readFile ('./Paginas/pagina.html', 'utf8', function(err, archivoLeido){ console.log(err);
                                                                            console.log(archivoLeido)});
