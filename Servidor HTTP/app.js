ec16194fad22f2d89fb8c368a67f656d4bf0d01b
CristianSantacruz <santacruz-guarquila@hotmail.com>
2016-12-07 14:51:50 -0500

    Commit4

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -3,6 +3,20 @@ var app = express();
 
 var fs = require('fs');
 
-//Primer paramatro es el path de la pagina html y el segundo parametro es la codificación
-fs.readFile ('./Paginas/pagina.html', 'utf8', function(err, archivoLeido){ console.log(err);
-                                                                           console.log(archivoLeido)});
+app.get('/', function (req, res) {
+    
+    console.log('1 Antes de leer');
+    
+    //Primer paramatro es el path de la pagina html y el segundo parametro es la codificación
+    fs.readFile ('./Paginas/pagina.html', 'utf8', function(err, archivoLeido){ console.log('3' + err);
+                                                                           console.log('4' + archivoLeido)});
+    
+    
+    res.send(archivoLeido);
+});
+
+    console.log('2 Parece que termino de leer');
+
+app.listen(5050, function () {
+  console.log('Escuchando en puerto5050');
+});
