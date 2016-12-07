4261eb0c3d0f9ad1aee04250f528f5f610317cd5
CristianSantacruz <santacruz-guarquila@hotmail.com>
2016-12-07 14:34:23 -0500

    Commit2

--- Servidor HTTP/app.js
+++ Servidor HTTP/app.js
@@ -1,53 +1,8 @@
 var express = require('express');
 var app = express();
 
-var usuarios = [{ id:1, nombre: 'Pepe', cedula: '12345' },
-                { id:2, nombre: 'Juan', cedula: '12345'},    
-                { id:3, nombre: 'Pedro', cedula: '12345'}];
+var fs require(fs);
 
-var contador = 3;
-
-
-app.get('/', function (req, res) {
-  res.send('Hola Mundo');
-});
-
-
-app.get('/TecnologiasWeb', function (req, res) {
-  res.send(usuarios);
-});
-
-
-app.post('/TecnologiasWeb/:name', function (req, res) {
-    
-    var parametros = req.params.name;
-    for(var i=0; i<usuarios.length; i++){
-        if(parametros==usuarios[i].id){
-            res.json(usuarios[i]);
-        }
-    } 
-    
-    res.send('No existe el Usuario');
-});
-
-
-app.get('/Usuarios', function (req, res) {
-  
-    if(!req.query.nombre){
-        res.send('No envió nombre');
-    }
-    
-    if(!req.query.cedula){
-        res.send('No envió cédula');
-    }
-    
-    var nuevoUsuario = { id:contador+1, nombre:req.query.nombre, cedula:req.query.cedula };
-    usuarios.push(nuevoUsuario);
-    contador = contador++;
-    res.json(nuevoUsuario);
-    
-});
-
-app.listen(5050, function () {
-  console.log('Escuchando en puerto5050');
-});
+//Primer paramatro es el path de la pagina html y el segundo parametro es la codificación
+fs.readFile ('./Paginas/pagina.html', 'utf8', function(err, archivoLeido){ console.log(error);
+                                                                           console.log(archivoLeido)});
